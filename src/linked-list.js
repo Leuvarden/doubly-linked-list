dd11c9ddda3ecfd75e8e62e34ab56c9c74f2198d
Maryna Leanovich <sollferino@gmail.com>
2017-01-27 14:21:35 +0300

    edit methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,55 +2,55 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-      this.length = 0;
-      this._head = null;
-      this._tail = null;
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
     }
 
     append(value) {
-      var node = new Node(value);
+        var node = new Node(value);
 
-      if (this.length) {
-          this._tail.next = node;
-          node.previous = this._tail;
-          this._tail = node;
+        if (this.length) {
+            this._tail.next = node;
+            node.previous = this._tail;
+            this._tail = node;
         } else {
-        this._head = node;
-        this._tail = node;
-      }
+            this._head = node;
+            this._tail = node;
+        }
 
-      this.length++;
+        this.length++;
 
-      return this;
-      }
+        return this;
+    }
 
     head() {
-      return (this.length > 0) ? this._head.data : null;
+        return (this.length > 0) ? this._head.data : null;
     }
 
     tail() {
-      return (this.length > 0) ? this._tail.data : null;
+        return (this.length > 0) ? this._tail.data : null;
     }
 
     at(index) {
-      var currentNode = this._head,
-      length = this.length,
-      count = 1;
+        var currentNode = this._head,
+            length = this.length,
+            count = 1;
 
-      while (count <= index) {
-      currentNode = currentNode.next;
-      count++;
-      }
+        while (count <= index) {
+            currentNode = currentNode.next;
+            count++;
+        }
 
-      return currentNode.data;
+        return currentNode.data;
     }
 
     insertAt(index, value) {
-      if (index < this.length) {
+        if (index < this.length) {
 
             var node = new Node(value);
 
-            var nodeCur = this.at(index);
+            var nodeCur = this.getNode(index);
             var nodePrev = nodeCur.previous;
 
             node.previous = nodePrev;
@@ -67,101 +67,96 @@ class LinkedList {
     }
 
     isEmpty() {
-      return (this._head === null) ? true : false;
+        return (this._head === null) ? true : false;
     }
 
     clear() {
-      this._head = null;
-      this._tail = null;
-      this.length = 0;
+        if (this._head) {
+            this._head = null;
+            this._tail = null;
+            this.length = 0;
 
-      return this;
+            return this;
+        } else {
+            return new LinkedList();
+        }
     }
 
-    deleteAt(position) {
-      var currentNode = this._head,
-           length = this.length,
-           count = 1,
-           beforeNodeToDelete = null,
-           nodeToDelete = null,
-           deletedNode = null;
+    deleteAt(index) {
 
-       if (length === 0 || position < 1 || position > length) {
-           return null;
-       }
+        if (index < this.length) {
 
+            if (this._head === this._tail) {
+                return new LinkedList();
+            } else {
 
-       if (position === 1) {
-           this._head = currentNode.next;
+                var nodeCur = this.getNode(index);
+                var nodePrev = nodeCur.previous;
+                var nodeNext = nodeCur.next;
 
-           if (!this._head) {
-               this._head.previous = null;
-           } else {
-               this._tail = null;
-           }
+                nodePrev.next = nodeNext;
+                nodeNext.previous = nodePrev;
+                nodeCur = null;
+            }
+
+            this.length--;
 
 
-       } else if (position === this.length) {
-           this._tail = this._tail.previous;
-           this._tail.next = null;
-
-       } else {
-           while (count < position) {
-               currentNode = currentNode.next;
-               count++;
-           }
-
-           beforeNodeToDelete = currentNode.previous;
-           nodeToDelete = currentNode;
-           afterNodeToDelete = currentNode.next;
-
-           beforeNodeToDelete.next = afterNodeToDelete;
-           afterNodeToDelete.previous = beforeNodeToDelete;
-           deletedNode = nodeToDelete;
-           nodeToDelete = null;
-       }
-
-       this.length--;
-
-       return this;
+            return this;
+        } else {
+            return new LinkedList();
+        }
     }
 
     reverse() {
-      var head = this._head,
-          nodeCur = this._head,
-          temp;
+        var head = this._head,
+            nodeCur = this._head,
+            temp;
 
-      this._tail = this._head;
+        this._tail = this._head;
 
 
-      while(nodeCur){
-        temp = nodeCur.next;
-        nodeCur.next = nodeCur.previous;
-        nodeCur.previous = temp;
-        if(!temp){
-        this._head = nodeCur;
+        while (nodeCur) {
+            temp = nodeCur.next;
+            nodeCur.next = nodeCur.previous;
+            nodeCur.previous = temp;
+            if (!temp) {
+                this._head = nodeCur;
+            }
+            nodeCur = temp;
         }
-        nodeCur = temp;
-      }
-      return this;
+        return this;
     }
 
     indexOf(value) {
-      var node = this._head;
-      var i = 0; var flag;
-      while (i != this.length) {
-          if (node.data === value) {
-              return i;
-          } else {
-            flag = -1;
-          }
-          node = node.next;
-          i++;
-      }
-      return flag;
-   }
+        var node = this._head;
+        var i = 0;
+        var flag;
+        while (i != this.length) {
+            if (node.data === value) {
+                return i;
+            } else {
+                flag = -1;
+            }
+            node = node.next;
+            i++;
+        }
+        return flag;
+    }
 
-   
+    getNode(index) {
+        var currentNode = this._head,
+            length = this.length,
+            count = 1;
+
+        while (count <= index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+
+        return currentNode;
+    }
+
 
 
 }
